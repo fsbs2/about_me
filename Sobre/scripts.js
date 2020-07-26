2b1b03e922df56dd5a9bb9101305bd71eca4bddc
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2020-07-26 19:44:58 -0300

    Arquivo finalizado

--- Sobre/scripts.js
+++ Sobre/scripts.js
@@ -3,8 +3,11 @@ const link = document.querySelector('.linked')
 
 link.addEventListener("click", function(){
     modalOverlay.classList.add('active')
+    modalOverlay.querySelector("iframe").src="https://www.youtube.com/embed/G_HhAP9D6KI"
 })
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src=""
 })
+
